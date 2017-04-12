713ff6f9f8e9936c06c2e0d9773ca30b7f39143f
Matthew Badcock <matthewlisterb@gmail.com>
2017-04-13 00:11:48 +0200

    added wander and teleport

---
+++ Teleport.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var target[] : Transform;
+
+function Start() {
+    
+    // target[] gameobjects  = GameObject.FindObjectsWithTag("teleport");
+}
+ 
+function Update () {
+ 
+}
+ 
+function OnTriggerEnter (col : Collider) {
+ 
+        if(col.gameObject.tag == "teleport") {
+                this.transform.position = target.position;
+        }
+}
